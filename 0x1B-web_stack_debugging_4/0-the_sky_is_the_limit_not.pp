ecc9ee185b279f921f3098ff8ffc733474a67c8a
hassanaitoundjar <hassanprof96@gmail.com>
2024-02-23 21:03:28 +0000

    task4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,18 +1,12 @@
-# This Puppet manifest is designed to optimize Nginx configuration for handling high load conditions
+# fix problem of high amount of requests
 
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => template('nginx/nginx.conf.erb'),
-  notify  => Service['nginx'],
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
-}
-
-exec { 'fix--for-nginx':
-  command     => '/usr/bin/nginx -s reload',
-  refreshonly => true,
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
